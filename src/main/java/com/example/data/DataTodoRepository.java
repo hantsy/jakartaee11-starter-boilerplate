1c2c66b9074f2c76bbada32031f1965971d7b0dd
hantsy <hantsy@gmail.com>
2025-07-18 11:18:16 +0800

    chore: clean codes

--- src/main/java/com/example/data/DataTodoRepository.java
+++ src/main/java/com/example/data/DataTodoRepository.java
@@ -4,8 +4,6 @@ import com.example.domain.Todo;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface DataTodoRepository extends CrudRepository<Todo, Long> {
 }
