0044ef0ed91c45fa8e259fa8296ef5fc847f346e
hantsy <hantsy@gmail.com>
2025-11-12 13:22:15 +0800

    chore: clean codes

--- src/main/java/com/example/data/DataTodoRepository.java
+++ src/main/java/com/example/data/DataTodoRepository.java
@@ -3,7 +3,9 @@ package com.example.data;
 import com.example.domain.Todo;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Repository;
+import jakarta.transaction.Transactional;
 
 @Repository
+@Transactional
 public interface DataTodoRepository extends CrudRepository<Todo, Long> {
 }
